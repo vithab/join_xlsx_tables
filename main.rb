e972c5f23a477b45795c93a0230541d1a6eb31c4
Vit <vit.bogachev@gmail.com>
2022-07-21 10:35:12 +0300

    Read name files

--- main.rb
+++ main.rb
@@ -3,29 +3,36 @@
 require 'roo'
 require 'write_xlsx'
 
-xlsx = Roo::Spreadsheet.open('./input_files/1.xlsx')
+file_names = []
 person_list = []
 
-# Получим массив строк столбца имён, начиная с 6ой строки
-name_list = xlsx.sheet(0).column(17)[5..].each do |row|
-	row
+Dir.chdir('input_files') do
+	file_names = Dir.glob('*.xlsx').each { |file_name| file_name }
 end
 
-# Получим массив строк столбца номеров, начиная с 6ой строки
-phone_list = xlsx.sheet(0).column(18)[5..].each do |row|
-	row
+file_names.each do |file_name|
+	xlsx = Roo::Spreadsheet.open("./input_files/#{file_name}")
+
+	# Получим массив строк столбца имён, начиная с 6ой строки
+	name_list = xlsx.sheet(0).column(17)[5..].each do |row|
+		row
+	end
+
+	# Получим массив строк столбца номеров, начиная с 6ой строки
+	phone_list = xlsx.sheet(0).column(18)[5..].each do |row|
+		row
+	end
+
+	# Получив данные в переменные name_list, phone_list, закрываем документ
+	xlsx.close
+
+	# Делаем двумерный массив по парам [[имя, номер], ...]
+	# для удобства записи построчно через итерацию при помощи гема write_xlsx
+	name_list.each_with_index do |name, index|
+		person = [name, phone_list[index] ]
+		person_list << person
+	end
 end
-
-# Получив данные в переменные name_list, phone_list, закрываем документ
-xlsx.close
-
-# Делаем двумерный массив по парам [[имя, номер], ...]
-# для удобства записи построчно через итерацию при помощи гема write_xlsx
-name_list.each_with_index do |name, index|
-	person = [name, phone_list[index] ]
-	person_list << person
-end
-
 # Делаем список уникальным
 person_list.uniq!
 
